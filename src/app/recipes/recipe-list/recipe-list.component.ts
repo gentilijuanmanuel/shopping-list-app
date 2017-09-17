20faea61663fd0eec1ea797a7ef52477692e2cd8
Juan Manuel Gentili <gentilijuanmanuel80974a@gmail.com>
2017-09-16 23:45:53 -0300

    Modificaciones en recetas

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,8 +8,9 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('Primera receta', 'Esto es sólo una prueba', 'http://www.seriouseats.com/recipes/assets_c/2016/05/20160503-fava-carrot-ricotta-salad-recipe-1-thumb-1500xauto-431710.jpg'),
-    new Recipe('Segunda receta', 'Esto es sólo una prueba', 'http://www.seriouseats.com/recipes/assets_c/2016/05/20160503-fava-carrot-ricotta-salad-recipe-1-thumb-1500xauto-431710.jpg')
+    new Recipe('Pollo con arroz', 'Tiempo promedio de cocción: 1 hs.', 'https://upload.wikimedia.org/wikipedia/commons/3/39/Arroz-con-Pollo.jpg'),
+    new Recipe('Fideos con salsa', 'Tiempo promedio de cocción: 30 min.', 'https://cdn.pixabay.com/photo/2014/10/22/15/20/noodles-498094_960_720.jpg'),
+    new Recipe('Milanesa napolitana', 'Tiempo promedio de cocción: 1 hs.', 'https://upload.wikimedia.org/wikipedia/commons/a/a9/Milanesa_napolitana.jpg')
   ];
   constructor() { }
 
