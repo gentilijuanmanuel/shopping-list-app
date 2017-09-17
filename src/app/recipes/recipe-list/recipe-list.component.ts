e06242a7ab987a61d353b76dd04590fc1c330d2d
Juan Manuel Gentili <gentilijuanmanuel80974a@gmail.com>
2017-09-16 23:01:20 -0300

    First commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Primera receta', 'Esto es sólo una prueba', 'http://www.seriouseats.com/recipes/assets_c/2016/05/20160503-fava-carrot-ricotta-salad-recipe-1-thumb-1500xauto-431710.jpg'),
+    new Recipe('Segunda receta', 'Esto es sólo una prueba', 'http://www.seriouseats.com/recipes/assets_c/2016/05/20160503-fava-carrot-ricotta-salad-recipe-1-thumb-1500xauto-431710.jpg')
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
