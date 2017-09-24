ebc84fb92ae1cf6cc6b68ed171efbe10994d2d17
Juan Manuel Gentili <gentilijuanmanuel80974a@gmail.com>
2017-09-24 20:22:50 -0300

    Nuevas funciones en lista de ccompr

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,20 @@
+export class Ingredient {
+    // La manera corta de hacerlo:
+
+    constructor(public name: string, public amount: number) {
+        
+    }
+
+
+
+    // La manera larga:
+    /*
+    public name: string;
+    public amount: number;
+
+    constructor(name: string, amout: number) {
+        this.name = name;
+        this.amount = amout;
+    }
+    */
+}
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Chocolate', 4),
+    new Ingredient('Harina', 6),
+    new Ingredient('Arroz', 2)
+  ];
+
 
   constructor() { }
 
